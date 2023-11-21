0eb0a4fd69c39aaacbfef70a94b3152c4b692d9a
teamp1 <teamp1@norlab.ca>
2023-11-21 16:13:32 -0500

    Launch the same node 2 times. One for obstacle detection and the other for the street names. They use their own config file (dicts are not the same size)

--- ros2_aruco/launch/aruco_recognition.launch.py
+++ ros2_aruco/launch/aruco_recognition.launch.py
@@ -5,18 +5,33 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
-    aruco_params = os.path.join(
+    aruco_params_5x5 = os.path.join(
         get_package_share_directory('ros2_aruco'),
         'config',
-        'aruco_parameters.yaml'
+        'aruco_parameters_5x5.yaml'
+        )
+    
+    aruco_params_4x4 = os.path.join(
+        get_package_share_directory('ros2_aruco'),
+        'config',
+        'aruco_parameters_4x4.yaml'
         )
 
-    aruco_node = Node(
+    aruco_node_obstacle = Node(
         package='ros2_aruco',
+        namespace='obstacle',
         executable='aruco_node',
-        parameters=[aruco_params]
+        parameters=[aruco_params_4x4]
+    )
+
+    aruco_node_street = Node(
+        package='ros2_aruco',
+        namespace='street',
+        executable='aruco_node',
+        parameters=[aruco_params_5x5]
     )
 
     return LaunchDescription([
-        aruco_node
+        aruco_node_obstacle,
+        aruco_node_street
     ])
